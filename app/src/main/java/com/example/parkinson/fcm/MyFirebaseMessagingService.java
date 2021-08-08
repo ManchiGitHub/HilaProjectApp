f180c969b513edd64947364ec170e6dc87f8fe57
Or <or_haifa@outlook.com>
2021-08-08 23:28:33 +0300

    push notifications

--- app/src/main/java/com/example/parkinson/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/parkinson/fcm/MyFirebaseMessagingService.java
@@ -1,16 +1,21 @@
 package com.example.parkinson.fcm;
 
+import android.app.ActivityManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
+import androidx.navigation.NavDeepLinkBuilder;
+
 import com.example.parkinson.R;
 import com.example.parkinson.data.UserRepository;
 import com.example.parkinson.features.main.MainActivity;
@@ -21,6 +26,9 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
@@ -28,6 +36,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FirebaseMessagingServce";
 
     private MessagingManager messagingManager;
+    String roomKey;
+    String contactName;
 
     public MyFirebaseMessagingService() {
 
@@ -54,6 +64,14 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
             notificationTitle = remoteMessage.getNotification().getTitle();
             notificationBody = remoteMessage.getNotification().getBody();
+
+            if(remoteMessage.getData().size() > 0)
+            {
+                Map<String, String> data = remoteMessage.getData();
+                roomKey = data.get("room_key");
+                contactName = data.get("contact_name");
+            }
+
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
@@ -69,7 +87,18 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         createNotificationChannel();
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Bundle bundle = new Bundle();
+        bundle.putString("room_key", roomKey);
+        bundle.putString("contact_name", contactName);
+
+        PendingIntent pendingIntent =  new NavDeepLinkBuilder(this)
+                .setComponentName(MainActivity.class)
+                     .setGraph(R.navigation.nav_main)
+                .setDestination(R.id.chatFragment)
+                .setArguments(bundle)
+                .createPendingIntent();
 
         Uri defaultSoundUri =
                 RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
@@ -83,9 +112,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri);
 
-
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(0, builder.build());
+
+
+//        if(checkApp())
+//        {
+//            notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//            notificationManager.notify(0, builder.build());
+//        }
 
     }
 
@@ -100,6 +135,22 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             channel.enableVibration(true);
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+
+    public boolean checkApp() {
+        ActivityManager am = (ActivityManager) this
+                .getSystemService(ACTIVITY_SERVICE);
+
+        // get the info from the currently running task
+        List<ActivityManager.RunningTaskInfo> taskInfo = am.getRunningTasks(1);
+
+        ComponentName componentInfo = taskInfo.get(0).topActivity;
+        if (componentInfo.getPackageName().equalsIgnoreCase("com.example.parkinson")) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
